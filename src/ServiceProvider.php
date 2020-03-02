9df8587b037e497c7b2fc06833b7ccec07c1a184
Andrew Clark <andrew.clark@techquity.co.uk>
2020-03-02 12:06:50 +0000

    Update compatibility with new Aero changes

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,14 +2,14 @@
 
 namespace Techquity\ProductUpsells;
 
-use Twig\TwigFunction;
 use Aero\Catalog\Models\Product;
-use Aero\Store\Http\Responses\ProductPage;
-use Aero\Store\Http\Responses\CartItemAdd;
-use Aero\Store\Twig\Extensions\TwigFunctions;
 use Aero\Common\Providers\ModuleServiceProvider;
+use Aero\DataPort\Commands\Pipelines\ImportProductCSVPipeline;
+use Aero\Store\Http\Responses\CartItemAdd;
+use Aero\Store\Http\Responses\ProductPage;
+use Aero\Store\Twig\Extensions\TwigFunctions;
 use Techquity\ProductCollections\ProductCollections;
-use Aero\DataPort\Commands\Pipelines\ProductCSVPipeline;
+use Twig\TwigFunction;
 
 class ServiceProvider extends ModuleServiceProvider
 {
@@ -18,8 +18,8 @@ class ServiceProvider extends ModuleServiceProvider
         ProductPage::extend(AttachProductUpsells::class);
         CartItemAdd::extend(AddProductUpsells::class);
 
-        ProductCSVPipeline::add('import', ImportProductUpsells::class);
-        
+        ImportProductCSVPipeline::extend(ImportProductUpsells::class);
+
         TwigFunctions::add(new TwigFunction('product_upsells', function (Product $product, $limit = null, $eager = null) {
             return $this->app->make(ProductCollections::class)->handle([
                 [
